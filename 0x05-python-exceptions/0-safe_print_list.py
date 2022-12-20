fca6e7c531e23af63ad1f72e808c69f2dc2ef013
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2022-12-19 17:51:53 -0800

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
-oo
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    n = 0
+    for m in range(x):
+        try:
+            print(my_list[m], end="")
+            n += 1
+        except IndexError:
+            break
+    print()
+    return 
