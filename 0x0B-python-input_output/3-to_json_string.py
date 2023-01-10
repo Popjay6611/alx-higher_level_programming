3aa0ef795976c6e75916a62f8f437ae08fababe6
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2023-01-10 09:45:25 -0800

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,11 @@
-oo
+#!/usr/bin/python3
+"""
+contain the JSON string
+"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
