e0cfcd5429b2d535f7b3eb0608f668ea005ddd14
Popoola Oyeniyi Joseph <110994414+Popjay6611@users.noreply.github.com>
2023-03-13 11:30:22 -0700

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/13-add.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-// Script that contains a visible function
-
-exports.add = function (a, b) {
-  return a + b;
-};
--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-// Script that prints "JavaScript is amazing".
-
-console.log(process.argv[2] + ' is ' + process.argv[3]);
